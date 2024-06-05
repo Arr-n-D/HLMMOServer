9dba1c82f9fcffdb49e4cfe785be528243e86297
Yannick Fricke <yannickfricke@googlemail.com>
2024-06-05 07:04:59 +0200

    refactor(core): added the GetEnvironmentVariable function
    Also refactored the Core.cpp file to use the new function so it will be
    more clear to the developer which environment variable is missing.
    
    Also formatted the Core.hpp and Core.cpp files + cleaned up unused
    imports.

--- src/Core.cpp
+++ src/Core.cpp
@@ -1,45 +1,38 @@
 #include <Core.hpp>
-#include <iostream>
-
-#include <chrono>
 #include <thread>
 
+#include "spdlog/spdlog.h"
+
 using namespace std::chrono_literals;
-
-
 
 Core::Core(){};
 
 Core::~Core(){};
 
 void Core::Initialize() {
-    std::cout << "Initializing Core" << std::endl;
+    spdlog::info( "Initializing Core" );
 
-    // std::string clientId = getenv( "DISCORD_CLIENT_ID" );
-    // std::string secret = getenv( "DISCORD_SECRET" );
-    // std::string redirect_uri = getenv( "DISCORD_REDIRECT_URI" );
+    std::string sClientId = GetEnvironmentVariable( "DISCORD_CLIENT_ID" );
+    std::string sSecret = GetEnvironmentVariable( "DISCORD_SECRET" );
+    std::string RedirectURI = GetEnvironmentVariable( "DISCORD_REDIRECT_URI" );
 
-    char const* clientId = getenv( "DISCORD_CLIENT_ID" );
-    char const* discordSecret = getenv( "DISCORD_SECRET" );
-    char const* redirectURI = getenv( "DISCORD_REDIRECT_URI" );
-    if ( clientId == NULL || discordSecret == NULL || redirectURI == NULL ) {
-        // exit if the environment variables are not set
-        std::cout << "Missing environment variables" << std::endl;
-       std::this_thread::sleep_for(5000ms);
-        exit( 1 );
-    } else {
-        std::string sClientId( clientId );
-        std::string sSecret( discordSecret );
-        std::string RedirectURI( redirectURI );
+    spdlog::info( "Client ID: {}", sClientId );
+    spdlog::info( "Client Secret: {}", sSecret );
+    spdlog::info( "Redirect URI: {}", RedirectURI );
 
-        std::cout << "Client ID: " << sClientId << std::endl;
-        std::cout << "Secret: " << sSecret << std::endl;
-        std::cout << "Redirect URI: " << RedirectURI << std::endl;
+    m_pDiscordAuth = new DiscordAuth( sClientId, sSecret, RedirectURI );
+    m_pNetworkManager = new Networking::NetworkManager( m_pDiscordAuth );
+    m_pNetworkManager->Init();
+    m_pNetworkManager->StartServer();
+}
 
-        m_pDiscordAuth = new DiscordAuth( sClientId, sSecret, RedirectURI );
-        m_pNetworkManager = new Networking::NetworkManager( m_pDiscordAuth );
-        m_pNetworkManager->Init();
-        m_pNetworkManager->StartServer();
-        //  ...
+std::string Core::GetEnvironmentVariable( std::string environmentVariableName ) {
+    const char* outValue = getenv( environmentVariableName.data() );
+
+    if ( outValue != NULL ) {
+        return std::string{ outValue };
     }
+
+    spdlog::critical( "Missing environment variable: {}", environmentVariableName );
+    exit( 1 );
 }
