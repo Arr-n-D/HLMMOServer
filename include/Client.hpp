1fea41812e3c52a05207323aa190646a8cc26d79
Yannick Fricke <yannickfricke@googlemail.com>
2024-06-08 00:11:30 +0200

    style(client): added missing newline

--- include/Client.hpp
+++ include/Client.hpp
@@ -3,7 +3,7 @@
 #include <steam/isteamnetworkingutils.h>
 #include <steam/steamnetworkingsockets.h>
 
-#include <boost/uuid/uuid.hpp> 
+#include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
 
